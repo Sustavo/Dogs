3b2985bb0999f206dc679aabc0f563ca2b30636f
Gustavo <gust4v0309@gmail.com>
2023-03-13 23:08:23 -0300

    Feat: Import Header, Footer, Home and Login to consume in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+import Login from "./components/Login/Login"
 
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
