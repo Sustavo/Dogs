0147cc3cbc712a5cb97ff00d687e1322a2db7834
Gustavo <gust4v0309@gmail.com>
2023-03-14 23:07:15 -0300

    Feat: create Header page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom"
-import styles from "./Header.module.css"
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
 
 export default function Header() {
-  return(
-    <div>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+  const { data, userLogout } = useContext(UserContext);
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        
+        <Link className={styles.login} to={data ? '/conta' : '/login'}>
+          {data ? 
+          (<div style={{display: 'flex'}}>
+            Olá, {data.nome}!
+            <button onClick={userLogout}>Sair</button>
+          </div>) : 
+          "Login / Criar"}
+        </Link>
       </nav>
-    </div>
-  )
+    </header>
+  );
 }
