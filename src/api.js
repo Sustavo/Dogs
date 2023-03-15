d086e00b1cb58e8624680fe1bb0cb8696773698d
Gustavo <gust4v0309@gmail.com>
2023-03-14 22:58:32 -0300

    Feat: Create 'TOKEN_VALIDATE_POST'

--- src/api.js
+++ src/api.js
@@ -13,4 +13,15 @@ export function TOKEN_POST(body) {
   }
 }
 
+export function TOKEN_VALIDATE_POST(token) {
+  return {
+    url: API_URL + '/jwt-auth/v1/token/validate',
+    options: {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    }
+  }
+}
 
