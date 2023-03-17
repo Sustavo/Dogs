f66333de55c306c8832398a7d6a8ca335f7e4b37
Gustavo <gust4v0309@gmail.com>
2023-03-17 20:27:48 -0300

    feat: create nem style do Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import LoginForm from "./LoginForm"
-import LoginCreate from "./LoginCreate"
-import LoginPasswordLost from "./LoginPasswordLost"
-import LoginPasswordReset from "./LoginPasswordReset"
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
 import { useContext } from "react";
 import { UserContext } from "../../UserContext";
+import styles from "./Login.module.css";
 
 export default function Login() {
   const { login } = useContext(UserContext);
 
-  if(login === true) return <Navigate to="/conta" />
+  if (login === true) return <Navigate to="/conta" />;
 
-  return(
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={<LoginCreate/>} />
-        <Route path="/perdeu" element={<LoginPasswordLost/>} />
-        <Route path="/resetar" element={<LoginPasswordReset/>} />
-      </Routes>
-    </div>
-  )
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/criar" element={<LoginCreate />} />
+          <Route path="/perdeu" element={<LoginPasswordLost />} />
+          <Route path="/resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  );
 }
