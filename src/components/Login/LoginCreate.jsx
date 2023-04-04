ff5003382077074f914825cdf5ddce3455e11bd7
Gustavo <gust4v0309@gmail.com>
2023-04-04 18:51:49 -0300

    Feat: LoginCreate form

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -1,5 +1,38 @@
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import useForm from '../../hooks/useForm'
+import { USER_POST } from '../../api';
+import { UserContext } from '../../UserContext'
+import { useContext } from 'react';
+
 export default function LoginCreate() {
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const { userLogin } = useContext(UserContext);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const {url, options} = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    })
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+
+  }
+
   return(
-    <div>LoginCreate</div>
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
